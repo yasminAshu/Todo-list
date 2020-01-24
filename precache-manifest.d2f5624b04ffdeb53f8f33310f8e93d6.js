96943d871f0f07d54fa214b4c78d43a92950c286
yasmin <yasminagwan98@gmail.com>
2020-01-24 23:51:29 +0530

    Updates

---
+++ precache-manifest.d2f5624b04ffdeb53f8f33310f8e93d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62268e103ecaa0bbb8b3f337e90d2b99",
+    "url": "/Todo-list/index.html"
+  },
+  {
+    "revision": "a0ca0a75e891499999ec",
+    "url": "/Todo-list/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6c6bd75947cf2bdf5eee",
+    "url": "/Todo-list/static/js/2.4094b5c9.chunk.js"
+  },
+  {
+    "revision": "a0ca0a75e891499999ec",
+    "url": "/Todo-list/static/js/main.4455c740.chunk.js"
+  },
+  {
+    "revision": "895e78035450c62a9efe",
+    "url": "/Todo-list/static/js/runtime~main.9eebd9a7.js"
+  }
+]);
